712ac4bbfef686ad102e4edad75def3f50468add
Dirk Boshoff <dirk.boshoff@capgemini.com>
2024-04-25 23:44:35 +0200

    Some text

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { WebsiteName } from "./../../config"
+  import Features from "./features.svelte"
 
   const features = [
     {
@@ -195,118 +196,37 @@
         class="text-4xl md:text-6xl font-bold px-2"
         style="line-height: 1.2;"
       >
-        The
         <span
           class="underline decoration-secondary decoration-4 md:decoration-[6px]"
-          >open source</span
-        >,
+          >Secure
+        </span>
+        your
         <span
           class="underline decoration-secondary decoration-4 md:decoration-[6px]"
-          >fast</span
-        >, and
+          >machine to machine
+        </span>communication with an
         <span
           class="underline decoration-secondary decoration-4 md:decoration-[6px]"
-          >free to host</span
         >
-        <span> SaaS template</span>
+          affordable, easy-to-implement
+        </span>solution
+        <!-- <span
+          class="underline decoration-secondary decoration-4 md:decoration-[6px]"
+        >
+          insightful billing
+        </span>and
+        <span
+          class="underline decoration-secondary decoration-4 md:decoration-[6px]"
+        >
+          metrics</span
+        > -->
       </div>
       <div class="mt-6 md:mt-10 text-sm md:text-lg">
-        Built with <a
-          href="https://kit.svelte.dev"
-          class="link font-bold"
-          target="_blank">SvelteKit</a
-        >,
-        <a href="https://supabase.com" class="link font-bold" target="_blank"
-          >Supabase</a
-        >,
-        <a href="https://stripe.com" class="link font-bold" target="_blank"
-          >Stripe</a
-        >,
-        <a href="https://tailwindcss.com" class="link font-bold" target="_blank"
-          >Tailwind</a
-        >,
-        <a href="https://daisyui.com" class="link font-bold" target="_blank"
-          >DaisyUI</a
-        >, and
-        <a
-          href="https://www.postgresql.org"
-          class="link font-bold"
-          target="_blank">Postgres</a
-        >
-      </div>
-      <div class="mt-6 md:mt-2">
-        <a href="https://github.com/CriticalMoments/CMSaasStarter">
-          <button class="btn btn-primary btn-sm px-6">★ us on Github</button>
-        </a>
-        <a
-          href="https://github.com/CriticalMoments/CMSaasStarter/tree/main#saas-starter"
-        >
-          <button class="btn btn-outline btn-primary btn-sm px-6 mt-3 mx-2"
-            >Read the Docs</button
-          >
-        </a>
+        with insightful billing and metrics
       </div>
     </div>
   </div>
 </div>
-<div class="min-h-[60vh]">
-  <div class="pt-20 pb-8 px-7">
-    <div class="max-w-lg mx-auto text-center">
-      <div
-        class="text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-accent"
-      >
-        Explore the Features
-      </div>
-      <div class="mt-6 text-xl font-bold">
-        And try them on this
-        <span
-          class="underline decoration-secondary decoration-[3px] md:decoration-[4px]"
-        >
-          fully functional demo
-        </span>
-      </div>
-    </div>
+<Features />
 
-    <div
-      class="flex gap-6 mt-12 max-w-[1064px] mx-auto place-content-center flex-wrap"
-    >
-      {#each features as feature}
-        <div class="card bg-white w-[270px] min-h-[300px] flex-none shadow-xl">
-          <div class="card-body items-center text-center p-[24px] pt-[32px]">
-            <div>
-              <svg
-                width="50px"
-                height="50px"
-                class="mb-2 mt-1"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-                {@html feature.svgContent}
-              </svg>
-            </div>
-            <h2 class="card-title">
-              {feature.name}
-            </h2>
-            <p class="text-sm">
-              {feature.description}
-            </p>
-            {#if feature.link}
-              <a
-                href={feature.link}
-                class="pb-4"
-                target={feature.newPage ? "_blank" : ""}
-              >
-                <button
-                  class="btn btn-xs btn-outline rounded-full btn-primary min-w-[100px]"
-                  >{feature.linkText ? feature.linkText : "Try It"}</button
-                >
-              </a>
-            {/if}
-          </div>
-        </div>
-      {/each}
-    </div>
-  </div>
-</div>
+<!-- Secure your machine to machine communication with a solution that is affordable, easy-to-implement with insightful billing and metrics  -->
